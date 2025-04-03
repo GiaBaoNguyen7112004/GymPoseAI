752a3b8fde50aa34b8aaf74a101d07c48e87aaae
BaoDev <giabao7112004@gmail.com>
2025-04-03 23:04:15 +0700

    init project backend

---
+++ backend/gympose/src/main/java/com/pbl5/gympose/GymposeApplication.java
@@ -0,0 +1,13 @@
+package com.pbl5.gympose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymposeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymposeApplication.class, args);
+	}
+
+}
